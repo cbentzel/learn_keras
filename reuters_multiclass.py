4f9c2ff243aa55aebaf7d7c1a2cc436f42e1f21d
Chris Bentzel <chris@bentzel.net>
2019-05-20 06:25:54 -0400

    Model building

--- reuters_multiclass.py
+++ reuters_multiclass.py
@@ -1,6 +1,11 @@
 """Following Section 3.5 in 'Deep Learning with Python'."""
 
 from keras.datasets import reuters
+from keras import layers
+from keras import losses
+from keras import metrics
+from keras import models
+from keras import optimizers
 import numpy as np
 
 def decode_to_text(word_index_array):
@@ -33,8 +38,18 @@ def prepare_data():
     y_test = to_one_hot(test_labels, 46)
     return x_train, x_test, y_train, y_test
 
+def build_model(depth=3, width=64, final_width=46):
+    model = models.Sequential()
+    model.add(layers.Dense(width, activation='relu', input_shape=(10000,)))
+    for i in range(depth - 2):
+        model.add(layers.Dense(width, activation='relu'))
+    model.add(layers.Dense(final_width, activation='softmax'))
+    model.compile(optimizer=optimizers.RMSprop(lr=0.001), loss=losses.binary_crossentropy, metrics=[metrics.binary_accuracy])
+    return model
+
 def main():
     x_train, x_test, y_train, y_test = prepare_data()
+    model = build_model()
 
 
 if __name__ == '__main__':
