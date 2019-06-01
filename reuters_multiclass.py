28ee12195306228e8a59dcd95f2609904a3c2f99
Chris Bentzel <chris@bentzel.net>
2019-06-01 06:09:12 -0400

    Add multiclass example

--- reuters_multiclass.py
+++ reuters_multiclass.py
@@ -1,4 +1,4 @@
-"""Following Section 3.5 in 'Deep Learning with Python'."""
+"""Following Section 3.5 in Deep Learning with Python."""
 
 from keras.datasets import reuters
 from keras import layers
@@ -6,6 +6,7 @@ from keras import losses
 from keras import metrics
 from keras import models
 from keras import optimizers
+import matplotlib.pyplot as plt
 import numpy as np
 
 def decode_to_text(word_index_array):
@@ -44,13 +45,57 @@ def build_model(depth=3, width=64, final_width=46):
     for i in range(depth - 2):
         model.add(layers.Dense(width, activation='relu'))
     model.add(layers.Dense(final_width, activation='softmax'))
-    model.compile(optimizer=optimizers.RMSprop(lr=0.001), loss=losses.binary_crossentropy, metrics=[metrics.binary_accuracy])
+    model.compile(optimizer=optimizers.RMSprop(lr=0.001), loss=losses.categorical_crossentropy, metrics=[metrics.categorical_accuracy])
     return model
+
+def train_model(model, x_train, y_train, epochs=20):
+    x_val = x_train[:1000]
+    partial_x_train = x_train[1000:]
+    y_val = y_train[:1000]
+    partial_y_train = y_train[1000:]
+    history = model.fit(partial_x_train, partial_y_train, epochs=epochs, batch_size=512, validation_data=(x_val, y_val))
+    return history
+
+def plot_loss(history, title='Loss'):
+    """Plot loss relative to epochs"""
+    history_dict = history.history
+    loss_values = history_dict['loss']
+    val_loss_values = history_dict['val_loss']
+    epochs = range(1, len(loss_values) + 1)
+    plt.figure()
+    plt.plot(epochs, loss_values, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
+    plt.title(title)
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+
+def plot_accuracy(history, title='Accuracy'):
+    """Plot accuracy relative to epochs"""
+    history_dict = history.history
+    print(history_dict.keys())
+    acc = history_dict['categorical_accuracy']
+    val_acc = history_dict['val_categorical_accuracy']
+    epochs = range(1, len(acc) + 1)
+    plt.figure()
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    plt.title(title)
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
 
 def main():
     x_train, x_test, y_train, y_test = prepare_data()
     model = build_model()
-
+    history = train_model(model, x_train, y_train, epochs=100)
+    model_narrow = build_model(depth=3, width=4, final_width=46)
+    history_narrow = train_model(model_narrow, x_train, y_train, epochs=100)
+    plot_loss(history)
+    plot_accuracy(history)
+    plot_loss(history_narrow, title='Loss Narrow')
+    plot_accuracy(history_narrow, title='Acc Narrow')
+    plt.show()
 
 if __name__ == '__main__':
     main()
